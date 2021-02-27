2391e8195fa66a73061e6b4461cc64a55549e9de
student <pratiksuvarna@gmail.com>
2021-02-27 17:08:50 +0000

    test

---
+++ src/main/java/com/example/TestPipe/TestPipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestPipe/TestPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestPipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
